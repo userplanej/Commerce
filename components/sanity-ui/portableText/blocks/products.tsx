e6e6c2d619de4f90bca2355faf8761fedb59759e
Lee Jaehyu <goodbright0@gmail.com>
2023-06-15 21:02:14 +0900

    add page-product swiper

--- components/sanity-ui/portableText/blocks/products.tsx
+++ components/sanity-ui/portableText/blocks/products.tsx
@@ -2,6 +2,10 @@ import type { PortableTextBlock } from '@portabletext/types';
 import clsx from 'clsx';
 import ProductModule from 'components/sanity-ui/modules/product';
 import { SanityModuleProducts } from 'lib/sanity/types';
+import { Autoplay } from 'swiper';
+import 'swiper/css/autoplay';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.min.css';
 
 type Props = {
   value: PortableTextBlock & SanityModuleProducts;
@@ -18,18 +22,28 @@ export default function ProductsBlock({ value }: Props) {
     <div
       className={clsx(
         'first:mt-0 last:mb-0', //
-        'my-8 grid grid-cols-1 gap-3',
-        multipleProducts ? 'md:grid-cols-2' : 'md:grid-cols-1'
+        'my-8  gap-3',
+        'w-[500px]'
       )}
     >
-      {value?.modules?.map((module) => (
-        <ProductModule
-          imageAspectClassName="aspect-[320/220]"
-          key={module._key}
-          layout={value.layout}
-          module={module}
-        />
-      ))}
+      <Swiper
+        modules={[Autoplay]}
+        speed={1000}
+        parallax={true}
+        autoplay={{ delay: 6500, disableOnInteraction: false }}
+        loop={true}
+        watchSlidesProgress={true}
+      >
+        {value?.modules?.map((module) => (
+          <SwiperSlide key={module._key}>
+            <ProductModule
+              imageAspectClassName="aspect-[320/220]"
+              layout={value.layout}
+              module={module}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
