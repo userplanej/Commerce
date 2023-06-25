338f2d8dcb4ed63fbaf482e800ccb64882fbc131
JAEHYU LEE <air-userplane@JAEHYUs-MacBook-Air.local>
2023-06-26 00:53:55 +0900

    added Kpi Grash

--- components/tremor-page.tsx
+++ components/tremor-page.tsx
@@ -75,13 +75,46 @@ const formatters: { [key: string]: any } = {
   Delta: (number: number) => `${usNumberformatter(number, 2)}%`
 };
 
+export type DailyPerformance = {
+  date: string;
+  Sales: number;
+  Profit: number;
+  Customers: number;
+};
+
+export const performance: DailyPerformance[] = [
+  {
+    date: '2023-05-01',
+    Sales: 900.73,
+    Profit: 173,
+    Customers: 73
+  },
+  {
+    date: '2023-05-02',
+    Sales: 1000.74,
+    Profit: 174.6,
+    Customers: 74
+  },
+  {
+    date: '2023-05-03',
+    Sales: 1100.93,
+    Profit: 293.1,
+    Customers: 293
+  },
+  {
+    date: '2023-05-04',
+    Sales: 1200.9,
+    Profit: 290.2,
+    Customers: 29
+  }
+];
 export function TremorSection({}: {}) {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const selectedKpi = kpiList[selectedIndex]!;
 
   const areaChartArgs = {
     className: 'mt-5 h-72',
-    data: performance as unknown as number[],
+    data: performance,
     index: 'date',
     categories: [selectedKpi],
     colors: ['blue'] as Color[],
@@ -91,71 +124,96 @@ export function TremorSection({}: {}) {
   };
 
   return (
-    <Container className="grid  h-full w-full grid-cols-6 grid-rows-6 gap-10 ">
-      <div className="col-span-2 row-span-6 mx-6 mt-3  max-w-lg  ">
-        <h1> Tremor demo</h1>
-      </div>
+    <Container className="flex h-full w-full flex-col">
+      <div className=" mt-3 inline-block  h-[60px] w-full overflow-visible bg-white ">&nbsp;</div>
+      <div className="ml-9 mt-12 flex flex-row">
+        <div className=" dashboard-scrollbar  mt-3 h-[523px] w-[169px] overflow-auto">
+          <h3 className="text-xl font-medium tracking-widest  ">overview</h3>
+          <h3 className="text-xl font-medium tracking-widest">insights</h3>
+          <h3 className="text-xl font-medium tracking-widest">campaigns</h3>
+          <h3 className="text-xl font-medium tracking-widest">audiences</h3>
+          <h3 className="text-xl font-medium tracking-widest">assetreport</h3>
+          <h3 className="text-xl font-medium tracking-widest">overview</h3>
+          <h3 className="text-xl font-medium tracking-widest">insights</h3>
+          <h3 className="text-xl font-medium tracking-widest">campaigns</h3>
+          <h3 className="text-xl font-medium tracking-widest">audiences</h3>
+          <h3 className="text-xl font-medium tracking-widest">assetreport</h3>
+          <h3 className="text-xl font-medium tracking-widest">overview</h3>
+          <h3 className="text-xl font-medium tracking-widest">insights</h3>
+          <h3 className="text-xl font-medium tracking-widest">campaigns</h3>
+          <h3 className="text-xl font-medium tracking-widest">audiences</h3>
+          <h3 className="text-xl font-medium tracking-widest">assetreport</h3>
+          <h3 className="text-xl font-medium tracking-widest">overview</h3>
+          <h3 className="text-xl font-medium tracking-widest">insights</h3>
+          <h3 className="text-xl font-medium tracking-widest">campaigns</h3>
+          <h3 className="text-xl font-medium tracking-widest">audiences</h3>
+          <h3 className="text-xl font-medium tracking-widest">assetreport</h3>
+          <h3 className="text-xl font-medium tracking-widest">overview</h3>
+          <h3 className="text-xl font-medium tracking-widest">insights</h3>
+          <h3 className="text-xl font-medium tracking-widest">campaigns</h3>
+          <h3 className="text-xl font-medium tracking-widest">audiences</h3>
+          <h3 className="text-xl font-medium tracking-widest">assetreport</h3>
+        </div>
 
-      <div className="col-span-4  row-span-6  mt-3 px-12 py-12 sm:mx-2">
-        <Title>Dashboard</Title>
-        <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
+        <div className="grid-rows-12 grid flex-1 grid-cols-12 ">
+          <div className="row-span-12  col-span-12 mt-3 items-center justify-center overflow-scroll  px-6 py-0 sm:mx-2">
+            <Title>Dashboard</Title>
+            <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
 
-        <TabGroup className="mt-6">
-          <TabList>
-            <Tab>Overview</Tab>
-            <Tab>Detail</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <Grid numItemsMd={2} numItemsLg={3} className="mt-6 gap-6">
-                {kpiData.map((item) => (
-                  <KpiCard key={item.title} item={item} />
-                ))}
-              </Grid>
-              <div className="mt-6 block">
-                <Card>
-                  <div className="h-80">
-                    <div className="justify-between md:flex">
-                      <div>
-                        <Flex className="space-x-0.5" justifyContent="start" alignItems="center">
-                          <Title> Performance History </Title>
-                          <Icon
-                            icon={InformationCircleIcon}
-                            variant="simple"
-                            tooltip="Shows daily increase or decrease of particular domain"
-                          />
-                        </Flex>
-                        <Text> Daily change per domain </Text>
+            <TabGroup className="mt-0">
+              <TabList>
+                <Tab>Overview</Tab>
+                <Tab>Detail</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <Grid numItemsMd={2} numItemsLg={3} className="mt-6 gap-6">
+                    {kpiData.map((item) => (
+                      <KpiCard key={item.title} item={item} />
+                    ))}
+                  </Grid>
+                  <div className="mt-6 block">
+                    <Card>
+                      <div className="h-80">
+                        <div className="justify-between md:flex">
+                          <div>
+                            <Flex
+                              className="space-x-0.5"
+                              justifyContent="start"
+                              alignItems="center"
+                            >
+                              <Title> Performance History </Title>
+                              <Icon
+                                icon={InformationCircleIcon}
+                                variant="simple"
+                                tooltip="Shows daily increase or decrease of particular domain"
+                              />
+                            </Flex>
+                            <Text> Daily change per domain </Text>
+                          </div>
+                          <div>
+                            <TabGroup index={selectedIndex} onIndexChange={setSelectedIndex}>
+                              <TabList color="gray" variant="solid">
+                                <Tab>Sales</Tab>
+                                <Tab>Profit</Tab>
+                                <Tab>Customers</Tab>
+                              </TabList>
+                            </TabGroup>
+                          </div>
+                        </div>
+                        {/*  그래프 패널 진행중 */}
+                        <div className="mt-8 hidden sm:block">
+                          <AreaChart {...areaChartArgs} />
+                        </div>
+                        {/*  그래프 패널 마지막 */}
                       </div>
-                      <div>
-                        <TabGroup index={selectedIndex} onIndexChange={setSelectedIndex}>
-                          <TabList color="gray" variant="solid">
-                            <Tab>Sales</Tab>
-                            <Tab>Profit</Tab>
-                            <Tab>Customers</Tab>
-                          </TabList>
-                        </TabGroup>
-                      </div>
-                    </div>
-                    {/*  그래프 패널 진행중 */}
-                    <div className="mt-8 hidden sm:block">
-                      <AreaChart {...areaChartArgs} />
-                    </div>
-                    {/*  그래프 패널 마지막 */}
+                    </Card>
                   </div>
-                </Card>
-              </div>
-            </TabPanel>
-
-            <TabPanel>
-              <div className="mt-6">
-                <Card>
-                  <div className="h-96" />
-                </Card>
-              </div>
-            </TabPanel>
-          </TabPanels>
-        </TabGroup>
+                </TabPanel>
+              </TabPanels>
+            </TabGroup>
+          </div>
+        </div>
       </div>
     </Container>
   );
